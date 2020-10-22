b00f32fc6db45aeddc1acf13b767dc7e7838bb46
HDonovan96 <harry.bd11@gmail.com>
2020-10-22 13:55:27 +0100

    Added start of project with single moveaeble cube

---
+++ Assets/Scripts/CubeSpinner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+using BeardedManStudios.Forge.Networking.Generated;
+
+public class CubeSpinner : CubeSpinnerBehavior
+{
+    Rigidbody rigidbody;
+
+    [Range(0.0f, 30.0f)]
+    public float speed = 15.0f;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (networkObject == null)
+        {
+            return;
+        }
+
+        if (!networkObject.IsOwner)
+        {
+            transform.position = networkObject.position;
+            transform.rotation = networkObject.rotation;
+            return;
+        }
+
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * speed * Time.deltaTime;
+
+        networkObject.position = transform.position;
+        networkObject.rotation = transform.rotation;
+    }
+}
